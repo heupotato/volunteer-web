a7b42381fc285fcd86d8d73a4484e2b6f8a804ae
LoanThai <thloanth.dut@gmail.com>
2021-08-07 15:45:45 +0700

    services

---
+++ src/services/EditService.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+const EVENT_API_BASE_URL = "http://localhost:8080/api/projects";
+
+class EventService {
+    getEvents() {
+        return axios.get(EVENT_API_BASE_URL);
+    }
+        
+    createEvent(event) {
+        return axios.post(EVENT_API_BASE_URL, event);
+    }
+
+    getEvent(eventID) {
+        return axios.get(EVENT_API_BASE_URL + "/" + eventID);
+    }
+
+    deleteEvent(eventID){
+        return axios.delete(EVENT_API_BASE_URL + "/" + eventID); 
+    }
+
+    updateEvent(eventID, newEvent){
+        return axios.put(EVENT_API_BASE_URL + "/" + eventID, newEvent); 
+    }
+}
+
+export default new EventService()
