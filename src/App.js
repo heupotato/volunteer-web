48f9a97a04e06d7ea77f55658a04f06c5bd68a2e
LoanThai <thloanth.dut@gmail.com>
2021-08-08 12:02:08 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,30 +23,37 @@ import UpdateEvent from "./page/host/UpdateEvent";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Header />
-            <Switch>
-                <Route exact path="/about" component={About}></Route>
-                <Route exact path="/" component={Body}></Route>
-                <Route exact path="/register" component={Signup}></Route>
-                <Route exact path="/login" component={Signin}></Route>
-                <Route exact path="/search" component={Search}></Route>
-                <Route exact path="/profile" component={Profile} />
-                <Route exact path="/profile/:id" component={Profile}></Route>
-                <Route exact path="/update" component={UpdateProfile} />
-                <Route exact path="/password" component={UpdatePassword} />
-                <Route exact path = "/body" component= {Body} />
-                <Route exact path="/event" component={Favourites}></Route>
-                <Route exact path="/event/:id" component={Event}></Route>
-                <Route exact path="/newEvent" component={NewEvent}></Route>
-                <Route exact path="/homepage" component={Body}></Route>
-                <Route exact path="/registerEvent" component={Register}></Route>
-                {<PrivateRoute exact path="/admin" component={Admin} isAuthenticated={localStorage.getItem('role') === '3'}></PrivateRoute> }
-                {<PrivateRoute exact path="/user" component={BoardUser} isAuthenticated={localStorage.getItem('role') === '1'}></PrivateRoute> }
-            </Switch>
-            <Footer />
-        </BrowserRouter>
-    );
+        <div>
+            <UpdateEvent></UpdateEvent>
+        </div>
+    )
+
+    // return (
+    //     <BrowserRouter>
+    //         <Header />
+    //         <Switch>
+    //             <Route exact path="/about" component={About}></Route>
+    //             <Route exact path="/" component={Body}></Route>
+    //             <Route exact path="/register" component={Signup}></Route>
+    //             <Route exact path="/login" component={Signin}></Route>
+    //             <Route exact path="/search" component={Search}></Route>
+    //             <Route exact path="/profile" component={Profile} />
+    //             <Route exact path="/profile/:id" component={Profile}></Route>
+    //             <Route exact path="/update" component={Update} />
+    //             <Route exact path="/password" component={UpdatePassword} />
+    //             <Route exact path = "/body" component= {Body} />
+    //             <Route exact path = "/admin" component= {Admin} />
+    //             <Route exact path="/event" component={Favourites}></Route>
+    //             <Route exact path="/event/:id" component={Event}></Route>
+    //             <Route exact path="/newEvent" component={NewEvent}></Route>
+    //             <Route exact path="/homepage" component={Body}></Route>
+    //             <Route exact path="/registerEvent" component={Register}></Route>
+    //             {/* <PrivateRoute exact path="/admin" component={BoardAdmin} isAuthenticated={localStorage.getItem('role') === '3'}></PrivateRoute> */}
+    //             {/* <PrivateRoute exact path="/user" component={BoardUser} isAuthenticated={localStorage.getItem('role') === '1'}></PrivateRoute> */}
+    //         </Switch>
+    //         <Footer />
+    //     </BrowserRouter>
+    // );
 }
 
 export default App;
